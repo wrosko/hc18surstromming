cbfb01be30ceb46cdbb9df7c3c1112f37f092dfe
andrama <andrea_ramazzina@hotmail.it>
2018-03-01 20:50:26 +0100

    first commit of the delta pherormone level

--- ComputeDeltaPheromoneLevels.m
+++ ComputeDeltaPheromoneLevels.m
@@ -1,15 +1,26 @@
-function deltaPheromoneLevel = ComputeDeltaPheromoneLevels(pathCollection, pathLengthCollection)
-nPaths = size(pathCollection, 1);
-nCities = size(pathCollection, 2);
-deltaPheromoneLevel = zeros(nCities);
+function deltaPheromoneLevel = ComputeDeltaPheromoneLevels(pathCollection, totalScoreCollection)
+%{
+pathCollection:
+Each row contains the indexes of the ride. 
+If a car is moving from one node to another, just put it equal to the starting. 
+If it has finished to move, than it will just repeat the last cell.
 
-for iPath = 1:nPaths
-    thisPath = pathCollection(iPath, :);
-    thisPathLength = pathLengthCollection(iPath);
-    origin = thisPath(nCities);
-    for loc = 1:nCities
+totalScoreCollection:
+Total sum of (w+b) scored by that car
+%}
+
+nCar = size(pathCollection, 1);
+nMaxRide = size(pathCollection, 2);
+deltaPheromoneLevel = zeros(nMaxRide);
+
+for iCar = 1:nCar
+    thisPath = pathCollection(iCar, :);
+    thisPathScore = totalScoreCollection(iCar);
+    origin = thisPath(nMaxRide); % Assuming that if the car dsnt move than same value in the next cell
+    for loc = 1:nMaxRide
         dest = thisPath(loc);
-        deltaPheromoneLevel(dest, origin) = deltaPheromoneLevel(dest, origin) + 1/thisPathLength;
+        deltaPheromoneLevel(dest, origin) = deltaPheromoneLevel(dest, origin) + thisPathScore;
         origin = dest;
     end
+    deltaPheromoneLevel = deltaPheromoneLevel - diag(diag(deltaPheromoneLevel)); % Put diag elements = 0
 end
