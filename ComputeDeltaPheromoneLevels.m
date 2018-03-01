bf9b2afa8fea0c949762386b26ed1af3460f759a
andrama <andrea_ramazzina@hotmail.it>
2018-03-01 21:18:41 +0100

    Change to handle  cell array input

--- ComputeDeltaPheromoneLevels.m
+++ ComputeDeltaPheromoneLevels.m
@@ -1,23 +1,13 @@
-function deltaPheromoneLevel = ComputeDeltaPheromoneLevels(pathCollection, totalScoreCollection)
-%{
-pathCollection:
-Each row contains the indexes of the ride. 
-If a car is moving from one node to another, just put it equal to the starting. 
-If it has finished to move, than it will just repeat the last cell.
+function deltaPheromoneLevel = ComputeDeltaPheromoneLevels(pathCollection, totalScoreCollection,ridesNumber)
 
-totalScoreCollection:
-Total sum of (w+b) scored by that car
-%}
-
-nCar = size(pathCollection, 1);
-nMaxRide = size(pathCollection, 2);
-deltaPheromoneLevel = zeros(nMaxRide);
+nCar = length(pathCollection);
+deltaPheromoneLevel = zeros(ridesNumber);
 
 for iCar = 1:nCar
-    thisPath = pathCollection(iCar, :);
+    thisPath = cell2mat(pathCollection(iCar));
     thisPathScore = totalScoreCollection(iCar);
-    origin = thisPath(nMaxRide); % Assuming that if the car dsnt move than same value in the next cell
-    for loc = 1:nMaxRide
+    origin = thisPath(end); 
+    for loc = 1:length(thisPath)
         dest = thisPath(loc);
         deltaPheromoneLevel(dest, origin) = deltaPheromoneLevel(dest, origin) + thisPathScore;
         origin = dest;
